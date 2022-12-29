1038d65ae5b3fd28539b32f1bf344d503be16564
Krysia Sherburne <43474151+kldupless@users.noreply.github.com>
2022-12-29 15:02:30 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Explore the Planets`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
